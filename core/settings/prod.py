302c04543f5d1142a97b253c7147e441aec8965d
sti-munene <kinyuamunene.km@gmail.com>
2024-02-20 10:24:55 +0300

    chore: setup postgres db

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,9 +1,24 @@
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv("PGDATABASE")),
+        'USER': str(os.getenv("PGUSER")),
+        'PASSWORD': str(os.getenv("PGPASSWORD")),
+        'HOST': str(os.getenv("PGHOST")),
+        'PORT': str(os.getenv("PGPORT")),
+    }
+}
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000/",
-    "http://127.0.0.1:3000/",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+ALLOWED_HOSTS = [
+    'localhost'
 ]
