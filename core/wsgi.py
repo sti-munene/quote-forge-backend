e780a46c17e1c191b132158dc950d27ffc8d0ea9
sti-munene <kinyuamunene.km@gmail.com>
2024-02-24 20:50:14 +0300

    refactor: load settings file properly

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,14 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
-from pathlib import Path
-import os
-
-env_file_path =  Path(__file__).resolve().parent.parent
-dotenv_path = os.path.join(env_file_path, ".env")
-load_dotenv(dotenv_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", str(os.getenv("SETTINGS_MODULE")))
 application = get_wsgi_application()
