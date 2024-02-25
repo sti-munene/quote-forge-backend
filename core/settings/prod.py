40493fce13f86b80fff0a02dc7f1e036bc87a997
sti-munene <kinyuamunene.km@gmail.com>
2024-02-25 13:17:38 +0300

    chore: add frontend domain to cors allowed origins

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -17,6 +17,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://quote-forge-frontend.vercel.app",
 ]
 
 ALLOWED_HOSTS = [
