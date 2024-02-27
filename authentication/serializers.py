45722578ba3e12e4bdc902e39761dc0c88aca6a2
sti-munene <kinyuamunene.km@gmail.com>
2024-02-27 13:42:18 +0300

    feat: setup onboarding

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from business.models import Business
 
 User = get_user_model()
 
 class UserDetailsSerializer(serializers.ModelSerializer):
+    has_completed_onboarding = serializers.SerializerMethodField()
+
+    def get_has_completed_onboarding(self, obj):
+        try:
+            Business.objects.get(user=obj)
+            return True
+        except Business.DoesNotExist:
+            return False
+    
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'has_completed_onboarding')
         read_only_fields = ('id', 'email')
